f2f46b40ae16c3f6f412acefe04d5afbf8ba9949
mosespace <kisakyemoses50@gmail.com>
2023-03-28 00:43:29 +0300

    initial commit and updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -17,7 +17,7 @@ import {
   docker,
   meta,
   starbucks,
-  tesla,
+  typify,
   shopify,
   carrent,
   jobit,
@@ -116,11 +116,11 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
+    title: "Frontend Team Lead",
+    company_name: "Mihasoft",
     icon: starbucks,
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    iconBg: "#ffff",
+    date: "Jan 2023 - Current",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -129,16 +129,17 @@ const experiences = [
     ],
   },
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
-    iconBg: "#E6DEDD",
+    title: "Backend Developer",
+    company_name: "Typify Media",
+    icon: typify,
+    iconBg: "#000000",
     date: "Jan 2021 - Feb 2022",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Doing the backend on websites, using PHP and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
+      "reating, maintaining, testing, and debugging the entire back end of an applications as tasked",
     ],
   },
   {
