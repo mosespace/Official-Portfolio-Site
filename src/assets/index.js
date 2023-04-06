2d0c5a66d0e3ecf4cb9bb620f4c4b55d9f5de41a
mosespace <kisakyemoses50@gmail.com>
2023-04-06 11:09:59 +0300

    feat: imported live from assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,7 @@ import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
+import live from "./live.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
@@ -37,6 +38,7 @@ export {
   mobile,
   web,
   github,
+  live,
   menu,
   close,
   css,
